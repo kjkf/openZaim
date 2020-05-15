1991f675901723e38934a6f06e4613399b3e9679
a_umut <askar.umut.kal@gmail.com>
2020-05-16 05:58:49 +0600

    form elements

--- src/js/index.js
+++ src/js/index.js
@@ -52,4 +52,18 @@ $(document).ready( function() {
         const links = header__menu.querySelectorAll('.menu-item');
         links.forEach(link => link.addEventListener('click', e => btn_close.click()))
     });
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if ((form.checkValidity() === false)){
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
 });
