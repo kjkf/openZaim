d43e77100307f16754d495a2e8b7d57466a96f25
Bota <msbota@gmail.com>
2020-05-19 20:36:41 +0600

    smooth scroll is added

---
+++ src/js/getmethods.js
@@ -0,0 +1,13 @@
+const methods_block = document.getElementById("methods-block");
+if (methods_block !== null){
+  const blocks = document.querySelectorAll(".getmethods-block");
+
+  for(let i=0; i<blocks.length; i++) {
+    blocks[i].addEventListener("click", function(){
+        var activeblock = document.querySelector('.active');
+        activeblock.classList.remove("active");
+        blocks[i].classList.add("active");
+    });
+  }
+
+}
---
+++ src/js/smoothScroll.js
@@ -0,0 +1,100 @@
+const menu_links = document.querySelectorAll(".smooth_scroll");
+
+const urlHash = location.hash;
+window.addEventListener('DOMContentLoaded', (event) => {
+if (urlHash) {
+  setTimeout(() => { window.scrollTo(0, 0) }, 0)
+
+  const anchors = document.querySelectorAll('a[href^="#"]');
+  const header = document.querySelector('header').offsetHeight;
+  const startPosition = window.pageYOffset;
+  const duration = 1500;
+  const urlTarget = document.getElementById(urlHash.replace('#', ''));
+  const urlPosition = window.pageYOffset + urlTarget.getBoundingClientRect().top - header;
+  let start = null;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, urlPosition, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+
+
+  // window.scrollTo(0, easeInOutCubic(progress, startPosition, header, duration));
+  // if (progress < duration) window.requestAnimationFrame(step);
+
+  // window.scroll({
+  //   top: urlPosition,
+  //   behavior: 'smooth'
+  // });
+}
+});
+
+
+for(let i=0; i<menu_links.length; i++) {
+  menu_links[i].addEventListener("click", menuLinkClick);
+}
+
+function menuLinkClick(event) {
+  //removeActive();
+  event.currentTarget.classList.add('selected_link');
+  smoothScroll(event);
+}
+
+// function removeActive(){
+//   let selected = document.querySelectorAll(".selected_link");
+//   selected.forEach(el => {
+//     el.classList.remove('selected_link')
+//   })
+// }
+//
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const target = event.currentTarget.getAttribute("href")
+  const anotherPage = target.substring(0,target.search("#"));
+  const currloc = window.location.pathname
+  console.log('target = '+ target);
+  if (anotherPage == "" || currloc.search(anotherPage)>=0){
+  //console.log("event.currentTarget.getAttribute('href') = "+event.currentTarget.getAttribute("href"));
+    const targetId = target === "#" ? "header" : target.substring(target.search("#"), target.length);
+    const duration = 1500;
+    const targetPosition = document.querySelector(targetId).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition - 80;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+      if (progress < duration) window.requestAnimationFrame(step);
+    }
+  }else{
+    window.location.href = target// window.location(anotherPage)
+  }
+}
+
+// Easing Functions
+// function linear(t, b, c, d) {
+// 	return c*t/d + b;
+// };
+//
+// function easeInOutQuad(t, b, c, d) {
+// 	t /= d/2;
+// 	if (t < 1) return c/2*t*t + b;
+// 	t--;
+// 	return -c/2 * (t*(t-2) - 1) + b;
+// };
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
